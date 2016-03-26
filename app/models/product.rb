9bd7a85f08bd71a7f749d897ba4810c65dfa98cd
李沛秦 <lipeiqin@lipeiqindeMacBook-Pro.local>
2016-03-26 16:51:20 -0500

    add validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+	validates_presence_of :title, :description, :image_url
+	validates_numericality_of :price, greater_than_or_equal_to: 0.01
+	validates_uniqueness_of :title
+	validates_format_of :image_url, :with => %r{\.(gif|jpg|png)\Z}i, allow_blank: true,
+	                     :message =>"must be a URL for GIF, JPG or PNG image."
 end
